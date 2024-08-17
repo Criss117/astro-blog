5832ac7c13b48e8affff3b3f3d99738ac0fedb02
Cristian <95005685+Criss117@users.noreply.github.com>
2024-08-17 15:43:49 -0500

    env fixed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
+import { loadEnv } from "vite";
 import mdx from "@astrojs/mdx";
+
+const { SITE_URL } = loadEnv(process.env.NODE_ENV, process.cwd(), "");
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx()],
+  site: SITE_URL,
 });
--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -3,14 +3,12 @@ import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
 import { SITE_CONFIG } from "@/lib/config/site-config";
 
-export const GET: APIRoute = async () => {
-  const siteUrl = import.meta.env.SITE_URL;
-
+export const GET: APIRoute = async (context) => {
   const blog = await getCollection("blog");
   return rss({
     title: SITE_CONFIG.title,
     description: SITE_CONFIG.description,
-    site: siteUrl || "https://example.com",
+    site: context.site || "https://example.com",
     customData: `<language>es-ES</language>`,
     stylesheet: "/styles/rss.xsl",
     items: blog.map((post) => ({
